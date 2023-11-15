9098ae3115949ff8d7f6c18a07863a1a71304c50
UzTak <yujitakubo@gmail.com>
2023-11-15 15:23:19 -0800

    update

--- dev-yuji/data_gen.jl
+++ dev-yuji/data_gen.jl
@@ -3,7 +3,7 @@ using CSV, Random, DataFrames
 function create_obs_sites_csv(fname, x_max, y_max, num_rows::Int)
     # Create a DataFrame with specified columns
     df = DataFrame(x = round.(-x_max .+ (2*x_max) .* rand(num_rows), digits=2), 
-                   y = round.(-y_max .+ (2*y_max) .* rand(num_rows), digits=2), 
+                   y = round.(y_max .* rand(num_rows), digits=2), 
                    r = round.(rand(num_rows), digits=2))
     
     # Save the DataFrame to a CSV file
