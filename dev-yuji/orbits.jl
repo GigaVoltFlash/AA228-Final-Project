715418981f9eabea8b803753135950a804d47a20
UzTak <yujitakubo@gmail.com>
2023-11-16 13:20:08 -0800

    update

--- dev-yuji/orbits.jl
+++ dev-yuji/orbits.jl
@@ -47,6 +47,7 @@ function cart2koe(cart, mu)
     return [a,e,i,Ω,ω,M]
 end 
 
+
 # koe = [a,e,i,Ω,ω,M]
 function koe2cart(koe, mu)
     a = koe[1]
@@ -96,3 +97,78 @@ end
 function Rx(x)
     return [1 0 0; 0 cos(x) sin(x); 0 -sin(x) cos(x)]
 end
+
+
+
+
+function TR(s, a)
+
+    newstate = s 
+
+    max_x_ang = 30 
+    max_y_ang = 30
+
+    y_noise_mag = .01 
+    x_noise_mag = .01
+
+    x_dist = Normal(0, x_noise_mag)
+    y_dist = Normal(0, y_noise_mag)
+
+    obs_list = np.zeros(size(s.obs_list,1))
+
+    if a == 1
+        # No changes to rewards, etc
+        # println("Action 1: do nothing")
+
+        R = 0
+
+    else
+        target = s.target_list[a-1]
+        # calculate the required slew angle
+        x_angle = atand((target[1] - s.x) / s.alt) # not worried about using atan2 since these are smallish angles
+        # println(a[1])
+        y_angle = atand((target[2] - s.y) / s.alt)
+        # println(a[2])
+
+        if (abs(x_angle) > max_x_ang) || (abs(y_angle) > max_y_ang) 
+            # exceeded maximum angle
+
+            # println("Out of slew range. Doing nothing.")
+            R = 0
+
+        elseif s.observed_list[a-1] == 1
+            # already observed
+            # we may never reach this state, but including for now just in case
+
+            # println("Already observed this target. Doing nothing.")
+            R = 0
+
+        else
+            # this action is allowed
+
+            attitude = (x_angle, y_angle)    # new attitude after imaging target
+            R = target[3]    # getting the reward
+            # state.target_list[a-1][3] = 0   # set the reward in the image tuple to 0 so we don't image it again
+            terget_list[a-1] = 1 # set this to 1 to flag that it's been observed
+
+            # println("Slewing to target and imaging.")
+
+        end
+    end
+
+    # no matter the action, we continue down the orbit track
+    # for temporary test case: 
+    y_ += dydt + rand(y_dist)
+    x_ += rand(x_dist)
+
+    # println("Next time step")
+
+    # for final version: this is where we propagate dynamics TODO
+
+    # TODO: update the list of available targets and actions based on visible horizon
+
+    s_new = State(x,y,s.dydt,s.alt,s.target_list,obs_list) 
+
+    return s_new, R
+end
+
