4c867588faf2ad6db9abb730d6e3839a856fa0fe
GigaVoltFlash <anshukc@gmail.com>
2023-11-16 14:48:45 -0800

    Fixed MCTS, plots in the integrated notebook

--- dev-anshuk/state.jl
+++ dev-anshuk/state.jl
@@ -34,12 +34,10 @@ Action space:
 n+1 -> Image target n 
 """
 
-function TR(state, a)
+function TR(s, a)
 
-    newstate = copy(state) 
-
-    max_x_ang = 30 
-    max_y_ang = 30
+    max_x_ang = 15 
+    max_y_ang = 15
 
     y_noise_mag = .01 
     x_noise_mag = .01
@@ -47,18 +45,21 @@ function TR(state, a)
     x_dist = Normal(0, x_noise_mag)
     y_dist = Normal(0, y_noise_mag)
 
+    obs_list = deepcopy(s.observed_list)
+
     if a == 1
         # No changes to rewards, etc
         # println("Action 1: do nothing")
 
         R = 0
+        attitude = s.attitude
 
     else
-        target = newstate.target_list[a-1]
+        target = s.target_list[a-1]
         # calculate the required slew angle
-        x_angle = atand((target[1] - newstate.x) / newstate.alt) # not worried about using atan2 since these are smallish angles
+        x_angle = atand((target[1] - s.x) / s.alt) # not worried about using atan2 since these are smallish angles
         # println(a[1])
-        y_angle = atand((target[2] - newstate.y) / newstate.alt)
+        y_angle = atand((target[2] - s.y) / s.alt)
         # println(a[2])
 
         if (abs(x_angle) > max_x_ang) || (abs(y_angle) > max_y_ang) 
@@ -66,32 +67,32 @@ function TR(state, a)
 
             # println("Out of slew range. Doing nothing.")
             R = 0
+            attitude = s.attitude
 
-        elseif newstate.observed_list[a-1] == 1
+        elseif s.observed_list[a-1] == 1
             # already observed
             # we may never reach this state, but including for now just in case
 
             # println("Already observed this target. Doing nothing.")
             R = 0
+            attitude = s.attitude
 
         else
             # this action is allowed
 
-            newstate.attitude = (x_angle, y_angle)    # new attitude after imaging target
+            attitude = (x_angle, y_angle)    # new attitude after imaging target
             R = target[3]    # getting the reward
-            # state.target_list[a-1][3] = 0   # set the reward in the image tuple to 0 so we don't image it again
-            newstate.observed_list[a-1] = 1 # set this to 1 to flag that it's been observed
+            obs_list[a-1] = 1 # set this to 1 to flag that it's been observed
 
             # println("Slewing to target and imaging.")
 
         end
-
     end
 
     # no matter the action, we continue down the orbit track
     # for temporary test case: 
-    newstate.y += dydt + rand(y_dist)
-    newstate.x += rand(x_dist)
+    y_ = s.y + dydt + rand(y_dist)
+    x_ = s.x + rand(x_dist)
 
     # println("Next time step")
 
@@ -99,5 +100,7 @@ function TR(state, a)
 
     # TODO: update the list of available targets and actions based on visible horizon
 
-    return newstate, R
+    s_new = State(x_,y_,s.dydt,s.alt, attitude, s.target_list,obs_list) 
+
+    return s_new, R
 end
