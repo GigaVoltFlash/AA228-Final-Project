388727e63e93bf894d64c0a620e9983cc2e353a1
GigaVoltFlash <anshukc@gmail.com>
2023-12-07 00:23:57 -0800

    Putting scripts in src

---
+++ src/integrated_mcts_heuristic.jl
@@ -0,0 +1,229 @@
+println("Getting required packages")
+using Plots
+using CSV, Random, DataFrames
+using GLMakie
+using GeoDatasets
+using JLD2
+
+include("state.jl")
+include("../dev-yuji/heuristics.jl")
+
+println("Setting up problem")
+# SET THE DATASET YOU WANT TO TEST
+dataset = "obs_site_Earth_50.csv"
+# SET THE NUMBER OF TIMES YOU WANT TO RUN THIS
+num_runs = 1
+
+target_list, n_targets = create_target_list_3d(dataset)
+
+
+# constants
+Re = 6371;
+mu_E = 3.986004418e5; #km^3/m^2  
+slew_limit = 15
+
+# MCTS parameters
+gamma = 0.95 # discount factor for the MDP problem
+m = 500 # simulation count
+c = 4000 # exploration constant
+d = 30 # simulation depth
+d_r = 1 # rollout depth
+time_step = 30
+
+# reading in dataset for plotting in the future
+df = CSV.read(dataset, DataFrame)
+target_lambda = df.lambda
+target_phi = df.phi
+target_mean_reward = df.r_mean
+
+
+####################################### MCTS functions #######################################
+# A shortened representation of MDP that works for online planning (no full state-space, TR captures T and R)
+struct MDP 
+    gamma # Discount factor
+    A # Action space
+    TR # Transition model
+end 
+
+function rollout(P, s, d)
+    s_ = copy(s)
+    ret = 0.0 
+    for t in 1:d
+        a = heuristic_action(s, slew_limit)
+        s_, r = P.TR(s_, a)
+        ret += P.gamma^(t-1) * r
+    end 
+    return ret 
+end
+
+struct MonteCarloTreeSearch
+    P
+    N   # visit count
+    Q   # action-value estimate
+    d   # depth to go through for the Monte Carlo search
+    d_r # depth to go through the rollout at the end of the MCTS
+    m   # num of simulations
+    c   # exploration constant 
+    U   # value function estimate 
+end 
+
+function (π::MonteCarloTreeSearch)(s)
+    for k in 1:π.m
+        simulate!(π,s)
+    end 
+    return argmax(a->π.Q[(s,a)], π.P.A)
+end 
+
+function simulate!(π::MonteCarloTreeSearch, s, d=π.d)
+    if d <= 0 
+        return rollout(π.P, s, π.d_r)
+    end 
+    P, N, Q, c = π.P, π.N, π.Q, π.c
+    A, TR, gamma = P.A, P.TR, P.gamma
+    if !haskey(N,(s,first(A)))  # if (s,a) has never been visited
+        for a in A
+            N[(s,a)] = 0 
+            Q[(s,a)] = 0
+        end 
+        return rollout(π.P, s, π.d_r)   
+    end
+    
+    # if (s,a) has been visited
+    a = explore(π,s)
+    # println("State before TR in simulate \n", s)
+    s_, r = TR(s,a)
+    # println("State after TR in simulate \n", s)
+    q = r + gamma* simulate!(π, s_, d-1) 
+    N[(s,a)] += 1  # +1 for visit count 
+    Q[(s,a)] += (q-Q[(s,a)]) / N[(s,a)]  # The more you visited, the update of the Q will (usually) converge 
+    return q 
+end 
+
+bonus(Nsa, Ns) = Nsa==0 ? Inf : sqrt(log(Ns)/Nsa)
+
+function explore(π::MonteCarloTreeSearch, s)
+    A, N, Q, c = π.P.A, π.N, π.Q, π.c
+    Ns = sum(N[(s,a)] for a in A)
+    # objective = Q+bonus term 
+    # if there is no past visit, then that exploration is always prioritized 
+    return argmax(a -> Q[(s,a)] + c*bonus(N[(s,a)], Ns), A)  
+end 
+
+####################### RUNNING MCTS ####################### 
+mcts_reward_totals = zeros(num_runs)
+mcts_num_observed = zeros(num_runs)
+action_space =  1:n_targets+1 
+
+P = MDP(gamma, action_space, TR_orbit)
+
+koe0 = [
+        7057.0,  # a [km]
+        0.000879,  # e
+        deg2rad(98.12),  # i
+        deg2rad(255.09),  # Ω [rad]
+        deg2rad(225.37),  # ω [rad]
+        deg2rad(75.0),  # M [rad]
+    ]
+att = [0.,0.]
+dt = 0
+
+observed_list = zeros(n_targets)
+state = State3d(koe0, att, dt, target_list, observed_list)
+
+state_list_mcts = []
+action_list_mcts = []
+# Create an intial state
+for j in 1:num_runs
+    
+    observed_list = zeros(n_targets)
+    state = State3d(koe0, att, dt, target_list, observed_list)
+
+    Q = Dict{Tuple{State3d, Int}, Float64}()
+    N = Dict{Tuple{State3d, Int}, Int}()
+    U = Dict{State, Float64}()
+
+    # Q, N, and U are going to be difficult to define
+    mcts_run = MonteCarloTreeSearch(P, N, Q, d, d_r, m, c, U)
+
+    state_list_mcts = []
+    action_list_mcts = []
+    for t = 0:time_step:2000
+        # Push to the full list
+        state_copy = copy(state)
+        push!(state_list_mcts, state_copy)
+        # println("State copy")
+        # println(state_copy.observed_list)
+
+        # Run MCTS
+        state_for_mcts = copy(state)
+        mcts_action = mcts_run(state_for_mcts)
+
+        # Use the action
+        push!(action_list_mcts, mcts_action)
+        state, reward = TR_orbit(state, mcts_action, time_step)
+        mcts_reward_totals[j] += reward
+
+        # Calculate progress percentage
+        progress = t / 2000 * 100
+
+        # Create a string representation of the progress bar
+        bar_length = 20
+        bar_progress = round(Int, progress / (100 / bar_length))
+        bar_string = "[" * repeat("=" , bar_progress) * repeat(" " , bar_length - bar_progress) * "]"
+
+        # Print the progress bar
+        print("\rMCTS with Heuristic Rollouts Progress $j: $bar_string $(round(progress, digits=2))%")
+
+        # Sleep for a short duration to see the progress bar
+        sleep(0.1)
+    end
+    mcts_num_observed[j] = sum(state.observed_list)
+    println("")
+end
+
+println("##################### RESULTS #####################")
+println("Mean reward for MCTS with heuristic rollouts : ", mean(mcts_reward_totals))
+println("Mean no. of observations for MCTS with heuristic rollouts : ", mean(mcts_num_observed))
+println("##################### RESULTS #####################")
+
+println("Gathering data for plotting")
+# Gathering data for plotting
+mcts_observed_target_lambda = []
+mcts_observed_target_phi = []
+mcts_observed_target_reward = []
+for i in 1:length(state.observed_list)
+    if state.observed_list[i] == 1
+        push!(mcts_observed_target_lambda, target_lambda[i])
+        push!(mcts_observed_target_phi, target_phi[i])
+        push!(mcts_observed_target_reward, target_mean_reward[i])
+    end
+end
+mcts_observed_target_lambda = Float64.(mcts_observed_target_lambda)
+mcts_observed_target_phi = Float64.(mcts_observed_target_phi)
+mcts_observed_target_reward = Float64.(mcts_observed_target_reward)
+
+angs_list_mcts = []
+for i in 1:length(action_list_mcts)
+    # println(i)
+    action = action_list_mcts[i]
+    # println(action)
+    state = state_list_mcts[i]
+    # println(state)
+    if action > 1
+        target = state.target_list[action-1]
+        push!(angs_list_mcts, get_slew_angle(state.koe, target, state.dt))
+    else 
+        if i > 1
+            push!(angs_list_mcts, angs_list_mcts[i-1])
+        else
+            push!(angs_list_mcts, (0,0))
+        end
+    end
+end
+angs_list_mcts = collect(angs_list_mcts)
+cross_ang_des_mcts = [t[1] for t in angs_list_mcts]
+along_ang_des_mcts = [t[2] for t in angs_list_mcts];
+
+println("Saving variables to mcts_heuristic_runs.jld2")
+@save "mcts_heuristic_runs.jld2" state_list_mcts action_list_mcts along_ang_des_mcts mcts_observed_target_lambda mcts_observed_target_phi mcts_observed_target_reward mcts_num_observed mcts_reward_totals target_lambda target_phi target_mean_reward
+println("DONE")
---
+++ src/plotter.jl
@@ -0,0 +1,124 @@
+println("Getting required packages")
+using Plots
+using CSV, Random, DataFrames
+using GLMakie
+using GeoDatasets
+using JLD2
+include("../dev-yuji/heuristics.jl")
+include("state.jl")
+
+MARKER_SIZE_FACTOR = 25 # CHANGE TO WHAT LOOKS GOOD
+# constants
+Re = 6371;
+mu_E = 3.986004418e5; #km^3/m^2  
+slew_limit = 15
+
+println("Loading in data")
+@load "heuristic_runs.jld2" state_list action_list cross_ang_des along_ang_des observed_target_lambda observed_target_phi observed_target_reward num_observed reward_totals target_lambda target_phi target_mean_reward
+@load "mcts_random_runs.jld2" state_list_mcts action_list_mcts cross_ang_des_mcts along_ang_des_mcts mcts_observed_target_lambda mcts_observed_target_phi mcts_observed_target_reward mcts_num_observed mcts_reward_totals target_lambda target_phi target_mean_reward
+
+println("Doing some last minute angle calcs")
+# Get some angles stuff
+x_angle_list = []
+y_angle_list = []
+index = 1:length(state_list)
+for i in 1:length(state_list)
+    push!(x_angle_list, state_list[i].attitude[1])
+    push!(y_angle_list, state_list[i].attitude[2])
+end
+
+x_angle_list_mcts = []
+y_angle_list_mcts = []
+index_mcts = 1:length(state_list_mcts)
+for i in 1:length(state_list_mcts)
+    push!(x_angle_list_mcts, state_list_mcts[i].attitude[1])
+    push!(y_angle_list_mcts, state_list_mcts[i].attitude[2])
+end
+
+println("Plotting")
+Plots.plot(index, action_list, label="Heuristic")
+Plots.plot!(index, action_list_mcts, title="Actions with MCTS", label="MCTS", show=true)
+savefig("action_plot.png")
+
+random_along = Plots.plot(index, [y_angle_list, along_ang_des], label=["Actual Angle" "Desired Angle"], title="Along track angle for Baseline")
+mcts_along = Plots.plot(index, [y_angle_list_mcts, along_ang_des_mcts], label=["Actual Angle" "Desired Angle"], title="Along track angle for MCTS")
+Plots.plot(random_along, mcts_along,layout=(1, 2), size=(1000, 400), ylabel="Degrees", xlabel="Time (s)", show=true)
+savefig("along_angles_plot.png")
+
+random_cross = Plots.plot(index, [x_angle_list, cross_ang_des], label=["State Heuristic" "Desired Heuristic"])
+mcts_cross = Plots.plot(index, [x_angle_list_mcts, cross_ang_des_mcts], label=["State MCTS" "Desired MCTS"])
+Plots.plot(random_cross, mcts_cross,layout=(1, 2), title="Cross track angle", size=(800, 400), show=true)
+savefig("cross_angles_plot.png")
+
+
+# ######### GLOBE PLOTTER #########
+koe = [state.koe for state in state_list]
+dts = [state.dt for state in state_list]
+state_ECEF = zeros(6, length(dts))
+state_ECI = zeros(6, length(dts))
+state_lambda = zeros(length(dts))
+state_phi = zeros(length(dts))
+for (i, koe) in enumerate(koe)
+    rv = koe2cart(koe, mu_E)
+    rv_ecef = ECI_to_ECEF(rv, dts[i])
+    state_lambda[i] = atand(rv_ecef[2], rv_ecef[1])
+    state_phi[i] = atand(rv_ecef[3], sqrt(rv_ecef[1]^2 + rv_ecef[2]^2))
+end
+
+fontsize_theme = Theme(fontsize = 20)
+set_theme!(fontsize_theme)
+layout = @layout [a b]
+f = Figure(size = (1000, 800), layout=layout)
+lon,lat,data = GeoDatasets.landseamask(;resolution='c',grid=5)
+ax1 = Axis(f[1, 1], aspect=DataAspect(), title = "Heuristic")
+sca1 =GLMakie.scatter!(ax1, target_lambda, target_phi, overdraw = true, color = :grey, markersize = MARKER_SIZE_FACTOR*target_mean_reward)
+sca2 =GLMakie.scatter!(ax1, observed_target_lambda , observed_target_phi, overdraw = true, color = :blue, markersize = MARKER_SIZE_FACTOR*observed_target_reward)
+GLMakie.contour!(ax1, lon, lat, data, levels=[0.5], color=:black, linewidth=0.5)
+path = lines!(ax1, state_lambda, state_phi, overdraw = true, color = :blue, linewidth = 2,)
+GLMakie.ylims!(ax1, low = -66, high = 66)
+GLMakie.xlims!(ax1, low = -140, high = -85)
+
+mcts_koe = [state.koe for state in state_list_mcts]
+mcts_dts = [state.dt for state in state_list_mcts]
+state_ECEF = zeros(6, length(mcts_dts))
+state_ECI = zeros(6, length(mcts_dts))
+mcts_state_lambda = zeros(length(mcts_dts))
+mcts_state_phi = zeros(length(mcts_dts))
+for (i, koe) in enumerate(mcts_koe)
+    rv = koe2cart(koe, mu_E)
+    rv_ecef = ECI_to_ECEF(rv, mcts_dts[i])
+    mcts_state_lambda[i] = atand(rv_ecef[2], rv_ecef[1])
+    mcts_state_phi[i] = atand(rv_ecef[3], sqrt(rv_ecef[1]^2 + rv_ecef[2]^2))
+end
+
+
+k = 10.580 
+Δλ = 10
+p_br = [-110 + k + Δλ, -60]
+p_bl = [-110 + k - Δλ, -60]
+p_tr = [-110 - k + Δλ,  60]
+p_tl = [-110 - k - Δλ,  60]
+
+l1 = lines!(ax1, [p_br[1], p_bl[1]], [p_br[2], p_bl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l2 = lines!(ax1, [p_tr[1], p_tl[1]], [p_tr[2], p_tl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l3 = lines!(ax1, [p_br[1], p_tr[1]], [p_br[2], p_tr[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l4 = lines!(ax1, [p_bl[1], p_tl[1]], [p_bl[2], p_tl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+
+
+lon,lat,data = GeoDatasets.landseamask(;resolution='c',grid=5)
+ax2 = Axis(f[1, 2], aspect=DataAspect(), title = "MCTS with Random Rollouts")
+GLMakie.scatter!(ax2, target_lambda, target_phi, overdraw = true, color = :grey, markersize = MARKER_SIZE_FACTOR*target_mean_reward)
+GLMakie.scatter!(ax2, mcts_observed_target_lambda , mcts_observed_target_phi, overdraw = true, color = :blue, markersize = MARKER_SIZE_FACTOR*mcts_observed_target_reward)
+GLMakie.contour!(ax2, lon, lat, data, levels=[0.5], color=:black, linewidth=0.5)
+lines!(ax2, mcts_state_lambda, mcts_state_phi, overdraw = true, color = :blue, linewidth = 2,)
+GLMakie.ylims!(ax2, low = -66, high = 66)
+GLMakie.xlims!(ax2, low = -140, high = -85)
+
+l1 = lines!(ax2, [p_br[1], p_bl[1]], [p_br[2], p_bl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l2 = lines!(ax2, [p_tr[1], p_tl[1]], [p_tr[2], p_tl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l3 = lines!(ax2, [p_br[1], p_tr[1]], [p_br[2], p_tr[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+l4 = lines!(ax2, [p_bl[1], p_tl[1]], [p_bl[2], p_tl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
+
+legend = Legend(f[1,3], [sca1, sca2, path, l1], ["Unobserved targets", "Observed targets", "Satellite Path", "Target Bounding Box"], framevisible=true)
+GLMakie.save("./map_plot.png", f)
+
