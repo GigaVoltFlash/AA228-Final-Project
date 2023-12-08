460f219a27e4607ea8f1b4222249b03e39203923
GigaVoltFlash <anshukc@gmail.com>
2023-12-07 22:56:32 -0800

    Plotter misc. changes

--- src/plotter.jl
+++ src/plotter.jl
@@ -14,8 +14,8 @@ mu_E = 3.986004418e5; #km^3/m^2
 slew_limit = 15
 
 println("Loading in data")
-@load "./src/heuristic_runs.jld2" state_list action_list cross_ang_des along_ang_des observed_target_lambda observed_target_phi observed_target_reward num_observed reward_totals target_lambda target_phi target_mean_reward
-@load "./src/mcts_random_runs1000.jld2" state_list_mcts action_list_mcts cross_ang_des_mcts along_ang_des_mcts mcts_observed_target_lambda mcts_observed_target_phi mcts_observed_target_reward mcts_num_observed mcts_reward_totals target_lambda target_phi target_mean_reward
+@load "../heuristic_runs1000.jld2" state_list action_list cross_ang_des along_ang_des observed_target_lambda observed_target_phi observed_target_reward num_observed reward_totals target_lambda target_phi target_mean_reward
+@load "../mcts_heuristic_runs1000.jld2" state_list_mcts action_list_mcts cross_ang_des_mcts along_ang_des_mcts mcts_observed_target_lambda mcts_observed_target_phi mcts_observed_target_reward mcts_num_observed mcts_reward_totals target_lambda target_phi target_mean_reward
 
 println("Doing some last minute angle calcs")
 # Get some angles stuff
@@ -121,5 +121,5 @@ l3 = lines!(ax2, [p_br[1], p_tr[1]], [p_br[2], p_tr[2]], overdraw=true, color=:o
 l4 = lines!(ax2, [p_bl[1], p_tl[1]], [p_bl[2], p_tl[2]], overdraw=true, color=:orange, linestyle=:solid, linewidth=2,)
 
 legend = Legend(f[1,3], [sca1, sca2, path, l1], ["Unobserved targets", "Observed targets", "Satellite Path", "Target Bounding Box"], framevisible=true)
-GLMakie.save("./map_plot.png", f)
+GLMakie.save("./comparison_1000.png", f)
 
